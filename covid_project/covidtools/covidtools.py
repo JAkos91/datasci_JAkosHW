a4815a7c95680318d9f7b3ec4b880f626e78715c
JAkos91 <akos.juhasz.etu@univ-lille.fr>
2021-05-07 11:47:11 +0000

    classes_hw

---
+++ covid_project/covidtools/covidtools.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import datetime
+
+class CovidData:
+      
+    def __init__(self, results, control, name):
+        self.results = results
+        self.control = control
+        self.name = name
+        
+        self.results = self.normalize()
+        
+    def __repr__(self):
+        return f"Covid data: {self.results}"
+    
+    def __len__(self):
+        return len(self.results)
+    
+    def __add__(self, other):
+        return [x + other for x in self.results]
+    
+    def normalize(self):
+        return [x / self.control for x in self.results]
+
+    def plot(self):
+        return px.line(self.results)
+     
+
+        
+    @classmethod
+    def changeType(cls, datatype):
+        cls.datatype =datatype
+    
+class Literature:
+    
+    def first():
+        pass
+
+class Image:
+    
+    pass
